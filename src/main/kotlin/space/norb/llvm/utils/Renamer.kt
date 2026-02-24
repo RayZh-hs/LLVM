b3acb4f63c433bad20d248603ab9673289a129a0
RayZh <rayzhangshanghai@hotmail.com>
2026-02-24 18:05:38 +0800

    refactor: make renamer a singleton object

--- src/main/kotlin/space/norb/llvm/utils/Renamer.kt
+++ src/main/kotlin/space/norb/llvm/utils/Renamer.kt
@@ -1,36 +1,34 @@
 package space.norb.llvm.utils
 
-class Renamer {
-    companion object {
-        private val nameCountMap = mutableMapOf<String?, Int>()
+object Renamer {
+    private val nameCountMap = mutableMapOf<String?, Int>()
 
-        fun another(baseName: String? = null): String {
-            if (baseName !in nameCountMap) {
-                nameCountMap[baseName] = 0
-            } else {
-                nameCountMap[baseName] = nameCountMap[baseName]!! + 1
-            }
-            val serial = nameCountMap[baseName]!!
-            return if (baseName == null)
-                serial.toString()
-            else
-                "${baseName}.$serial"
+    fun another(baseName: String? = null): String {
+        if (baseName !in nameCountMap) {
+            nameCountMap[baseName] = 0
+        } else {
+            nameCountMap[baseName] = nameCountMap[baseName]!! + 1
         }
+        val serial = nameCountMap[baseName]!!
+        return if (baseName == null)
+            serial.toString()
+        else
+            "${baseName}.$serial"
+    }
 
-        fun current(baseName: String? = null): String {
-            val serial = nameCountMap[baseName] ?: error("No name registered for base name: $baseName")
-            return if (baseName == null)
-                serial.toString()
-            else
-                "$baseName.$serial"
-        }
+    fun current(baseName: String? = null): String {
+        val serial = nameCountMap[baseName] ?: error("No name registered for base name: $baseName")
+        return if (baseName == null)
+            serial.toString()
+        else
+            "$baseName.$serial"
+    }
 
-        fun clear(baseName: String? = null) {
-            nameCountMap.remove(baseName)
-        }
+    fun clear(baseName: String? = null) {
+        nameCountMap.remove(baseName)
+    }
 
-        fun clearAll() {
-            nameCountMap.clear()
-        }
+    fun clearAll() {
+        nameCountMap.clear()
     }
 }
